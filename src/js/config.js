137cc9dc6e497717d3c2d1b6f7302260e53ea698
Matheus <=>
2023-08-10 08:56:32 -0300

    Complete Forkfy App

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const API_KEY = 'f48b9063-7675-42af-a30a-8f7c3cffd4ae';
+export const MODAL_CLOSE_SEC = 2.5;
